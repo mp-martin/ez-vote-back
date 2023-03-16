82af07ef1977162f40cf3d0efa88121384e9ece8
mp-martin <martin.paper@gmail.com>
2023-03-16 21:59:17 +0100

    Our backend can now receive and produce complete poll objects!

--- routers/poll.ts
+++ routers/poll.ts
@@ -3,7 +3,7 @@ import {ValidationError} from "../utils/error";
 import {QuestionRecord} from "../records/question.record";
 import {AnswerRecord} from "../records/answer.record";
 import {PollRecord} from "../records/poll.record";
-import {AnswerEntity, AnswerPool, CompletePoll, PollEntity, QuestionEntity} from "../types";
+import {AnswerEntity, CompletePoll, PollEntity, QuestionEntity} from "../types";
 
 export const pollRouter = Router();
 
@@ -13,42 +13,47 @@ pollRouter
 		const newPoll: CompletePoll = req.body;
 		const newPollRecord: PollEntity = newPoll.poll_header;
 
-		// try {
-		const pollId = await PollRecord.insert(newPollRecord);
-		newPoll.poll_header["poll_id"] = pollId;
-		newPoll.poll_body.map((element) => {
-			element.question_header.pollId = pollId;
-		});
+		try {
+			const pollId = await PollRecord.insert(newPollRecord);
+			newPoll.poll_header["poll_id"] = pollId;
+			newPoll.poll_body.map((element) => {
+				element.question_header.pollId = pollId;
+			});
 
-		const newQuestionRecords = newPoll.poll_body.map(element =>  {return {
-			element[0],
-		};});
+			const newQuestionRecords: QuestionEntity[] = newPoll.poll_body.map((element) => {
+				return {
+					...element.question_header
+				};
+			});
 
+			const questionIds = await Promise.all(newQuestionRecords.map(async (record) => await QuestionRecord.insert(record)));
 
+			newPoll.poll_body.map((element, index) => {
+				element.question_header.question_id = questionIds[index];
+			});
 
-		// const questionIds = await Promise.all(newQuestionRecords.map(async (record) => await QuestionRecord.insert(record)));
-		//
-		//
-		// const newAnswerRecords: AnswerEntity[][] = newPoll.poll_body.map((element, index) => {
-		// 	return {
-		// 		...element.answers,
-		// 		questionId: questionIds[index]
-		// 	};
-		// });
+			const newAnswerRecords = newPoll.poll_body.map((element) => {
+				return element.answers.map((answer) => {
+					return {
+						...answer,
+						questionId: element.question_header.question_id
+					};
+				});
+			}).flat();
 
-		// await Promise.all(newAnswerRecords.map(async (record) => await AnswerRecord.insert(record)));
+			await Promise.all(newAnswerRecords.map(async (record: AnswerEntity) => await AnswerRecord.insert(record)));
 
-		res.json({
-			newPoll,
-			newQuestionRecords,
-			// newAnswerRecords
-		});
-		// } catch(e) {
-		// 	res.json({
-		// 		"Success": false
-		// 	});
-		// 	throw new Error(e);
-		// }
+			res.json({
+				"success": true,
+				"newPollId": pollId
+			});
+
+		} catch(e) {
+			res.json({
+				"Success": false
+			});
+			throw new Error(e);
+		}
 
 	})
 
@@ -58,7 +63,7 @@ pollRouter
 		const questions = (await QuestionRecord.getByPollOfOrigin(pollId));
 
 		if (!poll) {
-			throw new ValidationError("Cannot get that poll`s details");
+			throw new ValidationError("Cannot get that poll`s details. Try again later.");
 		}
 
 		if (!questions) {
@@ -69,7 +74,7 @@ pollRouter
 			const answers = await AnswerRecord.getByQuestionOfOrigin(question.question_id);
 
 			if (!answers) {
-				throw new ValidationError("Cannot get questions from the poll. Please try again later.");
+				throw new ValidationError("Cannot get answers from the poll. Please try again later.");
 			}
 
 			return {
